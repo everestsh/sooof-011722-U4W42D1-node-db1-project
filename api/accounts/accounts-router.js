6cf451f01c7a23deb05592440fa4f326a0b67d5f
Mingjun Geng <sooofeve@gmail.com>
2022-02-14 12:13:28 -0800

    err test 2

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -4,8 +4,11 @@ const router = require('express').Router()
 // TEST ERR: http get :9000/api/accounts -v
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  // http get :7000/api/accounts -v
+  // res.send('TEST: GET /api/accounts')
   try{
-    throw new Error('argh!!!')
+    // throw new Error('argh!!!')
+    res.json([{},{}])
   }catch(err){
     next(err)
   }
@@ -13,18 +16,31 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  // http get :7000/api/accounts/1 -v
+  // res.send('TEST: GET /api/accounts/1') 
+  try{
+
+  }catch(err){
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  // http post :7000/api/accounts name=fff budget=ffffff -v
+  // res.json("TEST: create by endpoint")
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  // http put :7000/api/accounts/2 name=fff budget=ffffff -v
+  // res.json('TEST: update by endpoint') 
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  // http delete :7000/api/accounts/7  -v
+  // res.json('test delete by endpoint')
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
