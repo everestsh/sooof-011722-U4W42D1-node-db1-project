779b6c9d52999a2d063319135dcd8d6fbb4bbc73
Mingjun Geng <sooofeve@gmail.com>
2022-02-14 12:21:24 -0800

    endpoint work

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -8,7 +8,8 @@ router.get('/', (req, res, next) => {
   // res.send('TEST: GET /api/accounts')
   try{
     // throw new Error('argh!!!')
-    res.json([{},{}])
+    // res.json([{},{}])
+    res.json('get accounts ')
   }catch(err){
     next(err)
   }
@@ -19,7 +20,7 @@ router.get('/:id', (req, res, next) => {
   // http get :7000/api/accounts/1 -v
   // res.send('TEST: GET /api/accounts/1') 
   try{
-
+    res.json('get account by id')
   }catch(err){
     next(err)
   }
@@ -29,18 +30,33 @@ router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
   // http post :7000/api/accounts name=fff budget=ffffff -v
   // res.json("TEST: create by endpoint")
+  try{
+    res.json('post account')
+  }catch(err){
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
   // http put :7000/api/accounts/2 name=fff budget=ffffff -v
   // res.json('TEST: update by endpoint') 
+  try{
+    res.json('put account by id')
+  }catch(err){
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
   // http delete :7000/api/accounts/7  -v
   // res.json('test delete by endpoint')
+  try{
+    res.json('delete account by id')
+  }catch(err){
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
