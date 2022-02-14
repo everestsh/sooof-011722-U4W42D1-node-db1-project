33d97e6e38d3cf89434642e499873207073edd3f
Mingjun Geng <sooofeve@gmail.com>
2022-02-14 12:09:38 -0800

    err test

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,7 +1,14 @@
 const router = require('express').Router()
 
+
+// TEST ERR: http get :9000/api/accounts -v
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try{
+    throw new Error('argh!!!')
+  }catch(err){
+    next(err)
+  }
 })
 
 router.get('/:id', (req, res, next) => {
@@ -22,6 +29,9 @@ router.delete('/:id', (req, res, next) => {
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status|| 500).json({
+    message: err.message
+  })
 })
 
 module.exports = router;
