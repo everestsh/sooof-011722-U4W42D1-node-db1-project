4f3720850686af1511ca1f98656fcb7b0f735148
Mingjun Geng <sooofeve@gmail.com>
2022-02-25 08:18:02 -0800

    setrets

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const server = require("./api/server.js");
-require('dotenv').config()
+// require('dotenv').config()
 
-const PORT = process.env.PORT || 9000;
+// const PORT = process.env.PORT || 9000;
+const {PORT} = require('./secrets')
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
